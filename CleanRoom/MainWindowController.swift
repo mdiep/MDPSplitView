a409926ae93331c9c808d808989a787342b8f06e
Matt Diephouse <matt@diephouse.com>
2014-10-29 15:14:23 -0400

    Add a source list

--- CleanRoom/MainWindowController.swift
+++ CleanRoom/MainWindowController.swift
@@ -9,6 +9,20 @@
 import Cocoa
 
 class MainWindowController: NSWindowController, NSSplitViewDelegate {
+	@IBOutlet weak var splitView: NSSplitView!
+	
+	let sourceList = SourceListController(nibName: "SourceList", bundle: nil)!
+
+	override func awakeFromNib() {
+		let leftView = self.splitView.subviews[0] as NSView
+		let views = [
+			"sourceList": sourceList.view,
+		]
+		leftView.addSubview(sourceList.view)
+		leftView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[sourceList]|", options: nil, metrics: nil, views: views))
+		leftView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[sourceList]|", options: nil, metrics: nil, views: views))
+	}
+	
 	func splitView(splitView: NSSplitView, canCollapseSubview subview: NSView) -> Bool {
 		return subview == splitView.subviews[0] as NSObject
 	}
---
+++ CleanRoom/SourceListController.swift
@@ -0,0 +1,19 @@
+//
+//  SourceListController.swift
+//  CleanRoom
+//
+//  Created by Matt Diephouse on 10/29/14.
+//  Copyright (c) 2014 Matt Diephouse. All rights reserved.
+//
+
+import Cocoa
+
+class SourceListController: NSViewController {
+	override init?(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
+
+	required init?(coder: NSCoder) {
+	    super.init(coder: coder)
+	}
+}
