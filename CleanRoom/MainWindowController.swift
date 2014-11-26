3896a623f4840aebaec6c72b510618580c07cd9a
Rob Rix <rob.rix@github.com>
2014-11-26 14:06:44 -0500

    Revert "Don’t embed the source list here either."
    This reverts commit bd17baf28298202faa23b99cc83a6f30c848d547.

--- CleanRoom/MainWindowController.swift
+++ CleanRoom/MainWindowController.swift
@@ -17,9 +17,12 @@ class MainWindowController: NSWindowController, NSSplitViewDelegate {
 
 	override func awakeFromNib() {
 		let leftView = self.splitView.subviews[0] as NSView
-		sourceList.view.frame = leftView.frame
-		leftView.removeFromSuperview()
-		splitView.addSubview(sourceList.view, positioned: .Below, relativeTo: (splitView.subviews[0] as NSView))
+		let views = [
+			"sourceList": sourceList.view,
+		]
+		leftView.addSubview(sourceList.view)
+		leftView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[sourceList]|", options: nil, metrics: nil, views: views))
+		leftView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[sourceList]|", options: nil, metrics: nil, views: views))
 	}
 	
 	func splitView(splitView: NSSplitView, canCollapseSubview subview: NSView) -> Bool {
@@ -27,7 +30,7 @@ class MainWindowController: NSWindowController, NSSplitViewDelegate {
 	}
 	
 	@IBAction func toggleSourceList(sender: AnyObject?) {
-		let isOpen = !splitView.isSubviewCollapsed(splitView.subviews[0] as NSView)
+		let isOpen = !splitView.isSubviewCollapsed(sourceList.view.superview!)
 		let position = (isOpen ? 0 : lastWidth)
 		
 		splitView.wantsLayer = true
