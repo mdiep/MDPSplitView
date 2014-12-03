ad1313249a3cbf2a88e7d9d7bfc7793fac891d8d
Matt Diephouse <matt@diephouse.com>
2014-12-03 11:14:56 -0500

    Add some documentation

--- CleanRoom/MDPSplitView.h
+++ CleanRoom/MDPSplitView.h
@@ -9,8 +9,27 @@
 #import <Cocoa/Cocoa.h>
 
 
+/*!
+ An `NSSplitView` subclass that provides a method to animate the position of a
+ divider.
+ */
 @interface MDPSplitView : NSSplitView
 
+/*!
+ Set the position of a divider, possibly with an animation.
+ 
+ @discussion
+	This method will use the split view's animator proxy to animate by calling
+	`-setPosition:ofDividerAtIndex:`.
+ 
+ @param position
+	The new position of the divider.
+ @param dividerIndex
+	The index of the divider to position.
+ @param	animated
+	Whether the positioning should be animated.
+ */
 - (void)setPosition:(CGFloat)position ofDividerAtIndex:(NSInteger)dividerIndex animated:(BOOL)animated;
+
 
 @end
