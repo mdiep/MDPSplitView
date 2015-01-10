3e99942750c19bd1685dcb99587d3d546d259efa
Matt Diephouse <matt@diephouse.com>
2015-01-10 14:59:45 -0800

    Add an info pane on the right side

---
+++ MDPSplitViewDemo/InfoPaneController.swift
@@ -0,0 +1,19 @@
+//
+//  InfoPaneController.swift
+//  MDPSplitViewDemo
+//
+//  Created by Matt Diephouse on 1/10/15.
+//  Copyright (c) 2015 Matt Diephouse. All rights reserved.
+//
+
+import Cocoa
+
+class InfoPaneController: NSViewController {
+    override init?(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
