036f0172b8db3814db5a2ab251fc961e85c0800f
Rob Rix <rob.rix@github.com>
2015-02-17 12:37:02 -0500

    Add an autoresizing mask to each subview.
    This fixes the introduction of 0-width/-height
    NSAutoresizingMaskLayoutConstraints.

--- MDPSplitView.m
+++ MDPSplitView.m
@@ -133,6 +133,12 @@ static MDPSplitView *CommonInit(MDPSplitView *self)
         return [super defaultAnimationForKey:key];
 }
 
+- (void)didAddSubview:(NSView *)subview {
+	subview.autoresizingMask = NSViewWidthSizable | NSViewHeightSizable;
+	subview.translatesAutoresizingMaskIntoConstraints = YES;
+	[super didAddSubview:subview];
+}
+
 
 #pragma mark - API
 
