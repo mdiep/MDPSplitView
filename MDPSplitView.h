7dc591ea9ef3af5ac34690d3e35d15cbb86ea40c
Jan Weiß <jan@geheimwerk.de>
2017-08-21 15:26:02 +0200

    Adding documentation for the “-toggleSubview:…” methods.

--- MDPSplitView.h
+++ MDPSplitView.h
@@ -61,6 +61,30 @@ FOUNDATION_EXPORT const unsigned char MDPSplitViewVersionString[];
 - (BOOL)isAnimatingDividerAtIndex:(NSInteger)dividerIndex;
 
 
+/*!
+ Helper method to toggle a (vertically split) split view’s subview.
+ 
+ @discussion
+ This is a helper method to toggle a split view's subview.
+ It is animated by calling `-setPosition:ofDividerAtIndex:`.
+ There are two versions: one for split views with vertical dividers (this one)
+ and another for those with horizontal dividers.
+ 
+ @param subview
+ The subview to toggle.
+ @param dividerIndex
+ The index of the divider to position.
+ @param lastWidth
+ A reference to the variable you store the last width of the subview in.
+ @param animationDuration
+ The duration of the animation.
+ @param collapsesRightward
+ Whether you want to have the subview collapse rightward (or leftward, if NO).
+ @param widthConstraint
+ The width constraint you have applied to the subview so that it doesn’t become too narrow.
+ @param completionHandler
+ The completion handler that is called, once the animation has ended.
+ */
 - (void)toggleSubview:(NSView * _Nonnull)subview
          dividerIndex:(NSUInteger)dividerIndex
             lastWidth:(CGFloat * _Nonnull)lastWidth
@@ -69,6 +93,30 @@ FOUNDATION_EXPORT const unsigned char MDPSplitViewVersionString[];
       widthConstraint:(NSLayoutConstraint * _Nonnull)widthConstraint
     completionHandler:(nullable void (^)(BOOL isOpen))completionHandler;
 
+/*!
+ Helper method to toggle a (horizontally split) split view’s subview.
+ 
+ @discussion
+ This is a helper method to toggle a split view's subview.
+ It is animated by calling `-setPosition:ofDividerAtIndex:`.
+ There are two versions: one for split views with horizontal dividers (this one)
+ and another for those with vertical dividers.
+ 
+ @param subview
+ The subview to toggle.
+ @param dividerIndex
+ The index of the divider to position.
+ @param lastHeight
+ A reference to the variable you store the last height of the subview in.
+ @param animationDuration
+ The duration of the animation.
+ @param collapsesUpward
+ Whether you want to have the subview collapse upward (or downward, if NO).
+ @param heightConstraint
+ The height constraint you have applied to the subview so that it doesn’t become too narrow.
+ @param completionHandler
+ The completion handler that is called, once the animation has ended.
+ */
 - (void)toggleSubview:(NSView * _Nonnull)subview
          dividerIndex:(NSUInteger)dividerIndex
            lastHeight:(CGFloat * _Nonnull)lastHeight
